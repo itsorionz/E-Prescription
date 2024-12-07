468658eb2dc7de86d9c5a92bc5cc54cabd3d346c
iamAnamul Haque Sohel <sohel@placovu.com>
2024-12-07 10:57:02 +0600

    coment seed data

--- EPrescription.Repo/Migrations/Configuration.cs
+++ EPrescription.Repo/Migrations/Configuration.cs
@@ -25,27 +25,31 @@
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
 
-            var seedRoles = new List<Role>
-            {
-                new Role {RoleName = "Administrator", StatusFlag = 2},
-                new Role {RoleName = "Admin", StatusFlag = 1},
-            };
-            seedRoles.ForEach(s => context.Roles.AddOrUpdate(r => r.RoleName, s));
-            context.SaveChanges();
-            var seedRolePermissionList = new List<RoleTask>
-            {
-                new RoleTask{RoleId = context.Roles.FirstOrDefault(x => x.RoleName == "Administrator").Id, Task = "Global_SupAdmin"},
-            };
-            seedRolePermissionList.ForEach(s => context.RoleTasks.AddOrUpdate(u => u.Task, s));
-            context.SaveChanges();
+            //Seed Data
+            //-------------
 
-            var users = new List<User>
-            {
-                new User { FullName="Administrator",UserName = "admin",Password = "827ccb0eea8a706c4c34a16891f84e7b",RoleId = context.Roles.FirstOrDefault(x => x.RoleName == "Administrator").Id,SupUser = true, StatusFlag=2},//12345
-                new User { FullName="Development",UserName = "dev",Password = "827ccb0eea8a706c4c34a16891f84e7b",RoleId = context.Roles.FirstOrDefault(x => x.RoleName == "Administrator").Id,SupUser = true, StatusFlag=2}, //12345
-            };
-            users.ForEach(s => context.Users.AddOrUpdate(u => u.UserName, s));
-            context.SaveChanges();
+            //var seedRoles = new List<Role>
+            //{
+            //    new Role {RoleName = "Administrator", StatusFlag = 2},
+            //    new Role {RoleName = "Admin", StatusFlag = 1},
+            //};
+            //seedRoles.ForEach(s => context.Roles.AddOrUpdate(r => r.RoleName, s));
+            //context.SaveChanges();
+            //var seedRolePermissionList = new List<RoleTask>
+            //{
+            //    new RoleTask{RoleId = context.Roles.FirstOrDefault(x => x.RoleName == "Administrator").Id, Task = "Global_SupAdmin"},
+            //};
+            //seedRolePermissionList.ForEach(s => context.RoleTasks.AddOrUpdate(u => u.Task, s));
+            //context.SaveChanges();
+
+            //var users = new List<User>
+            //{
+            //    new User { FullName="Administrator",UserName = "admin",Password = "827ccb0eea8a706c4c34a16891f84e7b",RoleId = context.Roles.FirstOrDefault(x => x.RoleName == "Administrator").Id,SupUser = true, StatusFlag=2},//12345
+            //    new User { FullName="Development",UserName = "dev",Password = "827ccb0eea8a706c4c34a16891f84e7b",RoleId = context.Roles.FirstOrDefault(x => x.RoleName == "Administrator").Id,SupUser = true, StatusFlag=2}, //12345
+            //};
+            //users.ForEach(s => context.Users.AddOrUpdate(u => u.UserName, s));
+            //context.SaveChanges();
+
         }
     }
 }
